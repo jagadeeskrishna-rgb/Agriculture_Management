4ceb75a56d82c35770e670f8b2fec61e5d8f3d4f
jagadeeskrishna-rgb <jagadeeskrishna@gmail.com>
2026-08-17 20:34:10 +0530

    Delete source_code directory

--- source_code/farmops/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class FarmopsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "farmops"
-    verbose_name = "Agriculture Operations"
--- source_code/static/js/app.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelectorAll("form").forEach((form) => {
-    form.addEventListener("submit", () => form.querySelectorAll("button[type=submit], button:not([type])").forEach((btn) => btn.disabled = true));
-  });
-});
